c5112b762a9062977cc423ac0e3cdb66f2d81ebc
theycallmekevo <kevin2005tran@gmailclear>
2016-10-21 18:13:58 -0700

    creating card elements in JS

--- js/mainCopy.js
+++ js/mainCopy.js
@@ -7,3 +7,12 @@ var cardTwo = "queen";
 //Create 2 card variables with "king" as their values
 var cardThree = "king";
 var cardFour = "king";
+
+
+switch (cardFour) {
+	case ("king"):
+		alert("you've won!");
+	break;
+	default:
+		alert("keep playing!");
+}
